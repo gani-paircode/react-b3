52105c45d3dc09657542b31446eeb108cdc9b023
gani-paircode <gk.paircode@gmail.com>
2023-07-14 19:14:50 +0530

    router lesson 2 - setup + client side URL changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 
@@ -9,6 +10,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
